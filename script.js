3d107a037cc9c66aea594a544c53ba18e38e1275
Polar Bear <bballare21@gmail.com>
2019-09-24 23:40:13 -0700

    updated js css files

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
   let timeEl = document.querySelector(".time");
-  
+  let score = 0;
+  let current = 0;
+  let secondsLeft = 75;
+
+   //Setting the timer
   function setTime() {
-    var timerInterval = setInterval(function() {
+    let timerInterval = setInterval(function() {  
       secondsLeft--;
       timeEl.textContent = `${secondsLeft} seconds remaining`;
   
@@ -9,16 +13,76 @@
         clearInterval(timerInterval);
         sendMessage('Times Up');
       }
-  
-    }, 1000);
+      }, 1000);
   }
-  setTime();
-
-  let quest = 
 
   $('#start').on('click', function() {
-    secondsLeft = 75;
+    setTime();
     $('.dissapear').hide();
-
+    getQuestions()  
   })
+  
+  // Display the questions and choices to the quiz.
+  function getQuestions() {
+      let title = questions[current].title;
+      let choices = questions[current].choices;
+      $('#game').html(`<h4>${title}</h4>
+      ${getChoices(choices)}
+      `);
+  }
+
+  function getChoices(choices) {
+      let result = '';
+      for (let i = 0; i < choices.length; i++) {
+          result += `<p class="choice" data-answer="${choices[i]}">${choices[i]}</p>`;
+      }
+
+      return result;
+  }
+
+  //Go to next question when user input is entered.
+  $(document).on('click', '.choice', function() {
+      let selectedAnswer = $(this).attr('data-answer');
+      let correctAnswer = questions[current].answer;
+
+      if (correctAnswer === selectedAnswer) {
+          score++;
+          nextQuest();
+      }
+      else {
+          nextQuest()
+      }
+  })
+
+  function nextQuest() {
+    let isGameOver = (questions.length - 1) === current;
+    if (isGameOver) {
+        displayResult();
+    }
+    
+    else {
+        current++;
+        getQuestions();
+    }
+}
+
+  function displayResult() {
+      let result =
+      `<p>Game Over!</p>
+      <p>You got ${score} correct!</p>
+      <button class='btn btn-primary' id='reset'>Reset Game</button>`;
+
+      $('#game').html(result);
+  }
+  
+  $(document).on('click', '#reset', function () {
+    score = 0;
+    current = 0;
+    let secondsLeft = 75;
+
+    getQuestions()
+  })
+
+  localStorage.setItem('score', score)
+
   
